c1986cd65a4060ef1e51fbeab6f3857bea3d33fd
Ajith Kumar <ajith@ojcommerce.com>
2024-12-16 10:25:11 -0500

    Setup

---
+++ BMS.Core/Entities/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace BMS.Core.Entities
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+        public string? Genre { get; set; }
+        public DateTime PublishedYear { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
---
+++ BMS.Test.xUnit/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BMS.Test.xUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
